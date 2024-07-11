8e1693b2b890ac037fddf8806128611d207ca0e4
kleberwender <kleberwender@hotmail.com>
2024-07-11 16:37:24 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react()],
+  base: 'https://github.com/kleberwender/NLWJouney2024.git',
+  build: {
+    outDir: 'dist'
+  }
+});
